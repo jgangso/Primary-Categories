63c4e02161202934617b487f54f35f7373b9f44e
Jacob McKinney <jmckin02@gmail.com>
2016-04-28 16:15:51 -0400

    Update pc-meta-box.php
    added comments

--- pc-meta-box.php
+++ pc-meta-box.php
@@ -7,10 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 add_action( 'add_meta_boxes', 'pc_meta_box' );
 function pc_meta_box() {
 
+	// Retrieve all post types and add meta box to all post types, including custom post types
 	$post_types = get_post_types();
 
 	foreach ( $post_types as $post_type ) {
 
+		// Skip the "page" post type
 		if ( $post_type == 'page' ) {
 			continue;
 		}
@@ -20,8 +22,8 @@ function pc_meta_box() {
 			'Primary Category',
 			'pc_meta_box_content',
 			$post_type,
-			'side', // part of page
-			'high' // priority
+			'side',
+			'high'
 		);
 
 	}
@@ -34,16 +36,20 @@ function pc_meta_box_content() {
 
 	$primary_category = '';
 
+	// Retrieve data from primary_category custom field 
 	$current_selected = get_post_meta( $post->ID, 'primary_category', true );
 
+	// Set variable so that select element displays the set primary category on page load
 	if ( $current_selected != '' ) {
 		$primary_category = $current_selected;
 	}
 
+	// Get list of categories associated with post
 	$post_categories = get_the_category();
 
 	$html = '<select name="primary_category" id="primary_category">';
 
+	// Load each associated category into select element and display set primary category on page load
 	foreach( $post_categories as $category ) {
 		$html .= '<option value="' . $category->name . '" ' . selected( $primary_category, $category->name, false ) . '>' . $category->name . '</option>';
 	}
